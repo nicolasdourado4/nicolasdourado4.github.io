c6c08624697c3736596a9cb4191e6563a99f6e19
nicolasdourado4 <nicolasdourado04@gmail.com>
2024-12-01 14:54:16 -0400

    fix route order on app.js

--- server/app.js
+++ server/app.js
@@ -5,36 +5,30 @@ const dotenv = require('dotenv');
 const path = require('path');
 dotenv.config();
 
-const dbService= require('./dbService');
+const dbService = require('./dbService');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
-app.use(express.static(path.join(__dirname, '..')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'index.html'));
-});
-
+// Rotas da API primeiro
 app.get('/api/getAll', (req, res) => {
     const db = dbService.getDbServiceInstance()
     const result = db.GetAllData()
 
     result
-        .then(data => res.json({data: data})) //
+        .then(data => res.json({data: data}))
         .catch(err => console.log(err))
-})
+});
 
 app.get('/api/getCardData', (req, res) => {
     const db = dbService.getDbServiceInstance()
     const result = db.GetCardData()
 
     result
-        .then(data => res.json({data: data})) //
+        .then(data => res.json({data: data}))
         .catch(err => console.log(err))
-})
+});
 
 app.post('/api/insert', (req, res) => {
     const { email } = req.body;
@@ -44,19 +38,26 @@ app.post('/api/insert', (req, res) => {
     result
         .then(data => res.json({success: true}))
         .catch(err => console.log(err))
-})
+});
 
 app.post('/api/insertCard', (req, res) => {
-    const { title, description } = req.body
+    const { title, description } = req.body;
     const db = dbService.getDbServiceInstance()
     const result = db.insertCard(title, description)
 
     result
         .then(data => res.json({success: true}))
         .catch(err => console.log(err))
-})
+});
 
+// Servir arquivos estáticos depois das rotas da API
+app.use(express.static(path.join(__dirname, '..')));
 
-app.listen(process.env.PORT, () => {
+// Rota catch-all por último
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
+app.listen(process.env.PORT || 5000, () => {
     console.log("server started");
-})
+});
